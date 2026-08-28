5f1c36026f6ce09c48082bf38b04a70e17cb6ba8
Alexandre K. <alexandre.kcz.pro@gmail.com>
2026-08-28 15:51:36 +0200

    model and textures loading

--- src/engine/model.h
+++ src/engine/model.h
@@ -7,6 +7,7 @@
 
 typedef struct model {
     char model_name[32];
+    char model_texture[32];
     GsDOBJ2* model_data;
     vector3 position;
     vector3 rotation;
@@ -16,7 +17,10 @@ typedef struct model {
 } Model;
 
 void model_list_init(unsigned short max_length, unsigned short chunk_size);
-Model* model_register(unsigned char* model_name);
+Model* model_register(unsigned char* model_name, unsigned char* model_texture);
 void model_list_load();
 void model_create(unsigned long* image_data, Model* model);
 Array_List* get_model_list();
+
+void load_TMD(unsigned long* tmd, GsDOBJ2* obj);
+void load_texture(unsigned char* image_data);
--- src/engine/model/model.c
+++ src/engine/model/model.c
@@ -4,13 +4,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "../cd.h"
+
 Array_List* model_list = NULL;
 
 void model_list_init(unsigned short max_length, unsigned short chunk_size){
     model_list = array_list_create(max_length, chunk_size, sizeof(Model));
 }
 
-Model* model_register(unsigned char* model_name){
+Model* model_register(unsigned char* model_name, unsigned char* model_texture){
     if(!model_list){
         printf("WARNING : sprites_list has not been initialized, initializing with default values : max_length = 500, chunk_size = 5\n");
         model_list_init(500, 5);
@@ -31,5 +33,72 @@ Model* model_register(unsigned char* model_name){
 }
 
 void model_list_load(){
-    
+    cd_open();
+
+    for(int i = 0; i < model_list->length; i++){
+        unsigned long* tmd_buffer = NULL;
+        unsigned long* tim_buffer = NULL;
+
+        Model* model = *(Model**) array_list_get(model_list, i);
+
+        cd_read_file(model->model_name, &tmd_buffer);
+        cd_read_file(model->model_texture, &tim_buffer);
+
+        load_texture((unsigned char*)tim_buffer);
+        load_TMD(tmd_buffer, tim_buffer);
+    }
+
+    cd_close();
+}
+
+void load_TMD(unsigned long *tmd, GsDOBJ2 *obj){
+    //TODO : clean this mess
+
+    unsigned long* dop;
+    int object_number;
+
+    dop = tmd;
+    dop++;
+    GsMapModelingData(dop);
+
+    dop++;
+    object_number = *dop;
+
+    dop++;
+    GsMapModelingData(dop);
+
+    dop++;
+    object_number = *dop;
+
+    dop++;
+
+    for(int i = 0; i < object_number; i++){
+        GsLinkObject4((unsigned long) dop, &obj[i], i);
+    }
+}
+
+void load_texture(unsigned char* image_data){
+    GsIMAGE* tim_data;
+    RECT* rect;
+    RECT* crect;
+    tim_data = malloc3(sizeof(GsIMAGE));
+    GsGetTimInfo((unsigned long*)(image_data+4), tim_data);
+    rect = malloc3(sizeof(RECT));
+    crect = malloc3(sizeof(RECT));
+
+    rect->x = tim_data->px;
+    rect->y = tim_data->py;
+    rect->w = tim_data->pw;
+    rect->h = tim_data->ph;
+    LoadImage(rect, tim_data->pixel);
+
+    crect->x = tim_data->cx;
+    crect->y = tim_data->cy;
+    crect->w = tim_data->cw;
+    crect->h = tim_data->ch;
+    LoadImage(crect, tim_data->clut);
+
+    free3(rect);
+    free3(crect);
+    free3(tim_data);
 }
